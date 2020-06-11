17c6a9c0fdddbb66a9a4f2f593940d72a3c9f223
llx122631661 <llx122631661@aliyun.com>
2020-06-11 14:52:03 +0800

    分支测试提交

--- ruoyi-admin/src/main/java/com/ruoyi/Test.java
+++ ruoyi-admin/src/main/java/com/ruoyi/Test.java
@@ -8,6 +8,9 @@ public class Test {
         String[] b = a.split("");
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]+"-------------");
+            System.out.println(b[i]+"-------------");
+            System.out.println(b[i]+"-------------");
+            System.out.println(b[i]+"-------------");
         }
         for (String s : b) {
             System.out.println(s);
